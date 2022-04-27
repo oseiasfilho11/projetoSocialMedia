67556956e41e17c78a67242de0d62df93e51cd9c
oseiasfilho11 <oseiasfilho99@gmail.com>
2022-04-27 00:35:19 -0300

    Post - part 2

--- src/main/java/com/of/rest/webservices/restfulwebservice/user/UserResource.java
+++ src/main/java/com/of/rest/webservices/restfulwebservice/user/UserResource.java
@@ -1,8 +1,10 @@
 package com.of.rest.webservices.restfulwebservice.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -29,7 +32,14 @@ public class UserResource {
 
 	//@RequestMapping(method = RequestMethod.POST, path = "/users") <- usando o requestmapping
 	@PostMapping(path = "/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 		User userSaved = userService.save(user);
+		
+		//Cria uma nova uri com informações da requisição feita acrescentando apenas "/id" do objeto salvado e o enviado devolta na resposta.
+		//No cabeçalho http esse caminho vai ser o "Location" que corresponde o caminho onde o objeto se encontra 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userSaved.getId()).toUri();	
+		
+		//retorna o codigo 201 que significa que um novo recurso foi criado ao contrario enviaria apenas o 200
+		return ResponseEntity.created(uri).build();
 	}
 }
